44043f021c7ccdf5dfcc2fc2fe6621f566e7910d
Sadegh Rastgoo <sadegh.rsg@gmail.com>
2021-12-24 12:02:38 +0330

    make code readable and shorter

--- js/script.js
+++ js/script.js
@@ -6,109 +6,54 @@ const lastNameInp = document.getElementById("inp-lname");
 const emailInp = document.getElementById("inp-email");
 const passwordInp = document.getElementById("inp-password");
 
-submitBtn.addEventListener("click", function () {
-  // Check if Empty or not
-  if ([...firstNameInp.value].length === 0) {
-    const parentClass = firstNameInp.parentElement;
-    parentClass.classList.add("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.add("message");
-    document.querySelector(`.${parentClass.classList[0]} + p`).textContent =
-      "First Name cannot be empty";
-  } else {
-    const parentClass = firstNameInp.parentElement;
-    parentClass.classList.remove("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.remove("message");
-  }
-  if ([...lastNameInp.value].length === 0) {
-    const parentClass = lastNameInp.parentElement;
-    parentClass.classList.add("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.add("message");
-    document.querySelector(`.${parentClass.classList[0]} + p`).textContent =
-      "Last Name cannot be empty";
-  } else {
-    const parentClass = lastNameInp.parentElement;
-    parentClass.classList.remove("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.remove("message");
-  }
-  if ([...emailInp.value].length === 0) {
-    const parentClass = emailInp.parentElement;
-    parentClass.classList.add("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.add("message");
-    document.querySelector(`.${parentClass.classList[0]} + p`).textContent =
-      "Email cannot be empty";
-  } else {
-    const parentClass = emailInp.parentElement;
-    parentClass.classList.remove("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.remove("message");
-  }
-  if ([...passwordInp.value].length === 0) {
-    const parentClass = passwordInp.parentElement;
-    parentClass.classList.add("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.add("message");
-    document.querySelector(`.${parentClass.classList[0]} + p`).textContent =
-      "Password cannot be empty";
-  } else {
-    const parentClass = passwordInp.parentElement;
-    parentClass.classList.remove("invalid");
-    document
-      .querySelector(`.${parentClass.classList[0]} + p`)
-      .classList.remove("message");
-  }
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 
-  // Check if inputs correct ot not
-  const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+const invalidInput = function (message, parentClass) {
+  parentClass.classList.add("invalid");
+  document
+    .querySelector(`.${parentClass.classList[0]} + p`)
+    .classList.add("message");
+  document.querySelector(`.${parentClass.classList[0]} + p`).textContent =
+    message;
+};
+const validInput = function (parentClass) {
+  parentClass.classList.remove("invalid");
+  parentClass.classList.add("valid");
+  document
+    .querySelector(`.${parentClass.classList[0]} + p`)
+    .classList.remove("message");
+};
+
+submitBtn.addEventListener("click", function () {
+  // Check if inputs are empty or not
+  [...firstNameInp.value].length === 0
+    ? invalidInput("First Name cannot be empty", firstNameInp.parentElement)
+    : validInput(firstNameInp.parentElement);
+  [...lastNameInp.value].length === 0
+    ? invalidInput("Last Name cannot be empty", lastNameInp.parentElement)
+    : validInput(lastNameInp.parentElement);
+  [...emailInp.value].length === 0
+    ? invalidInput("Email cannot be empty", emailInp.parentElement)
+    : validInput(emailInp.parentElement);
+  [...passwordInp.value].length === 0
+    ? invalidInput("Password cannot be empty", passwordInp.parentElement)
+    : validInput(passwordInp.parentElement);
+
+  // Check if password and email inputs are correct or not
   if ([...emailInp.value].length >= 1) {
-    if (!validateEmail(emailInp.value)) {
-      const parentClass = emailInp.parentElement;
-      parentClass.classList.add("invalid");
-      document
-        .querySelector(`.${parentClass.classList[0]} + p`)
-        .classList.add("message");
-      document.querySelector(`.${parentClass.classList[0]} + p`).textContent =
-        "Looks like this is not an email";
-    } else {
-      const parentClass = emailInp.parentElement;
-      parentClass.classList.remove("invalid");
-      document
-        .querySelector(`.${parentClass.classList[0]} + p`)
-        .classList.remove("message");
-    }
+    !validateEmail(emailInp.value)
+      ? invalidInput("Looks like this is not an email", emailInp.parentElement)
+      : validInput(emailInp.parentElement);
   }
   if ([...passwordInp.value].length >= 1) {
-    if ([...passwordInp.value].length <= 8) {
-      const parentClass = passwordInp.parentElement;
-      parentClass.classList.add("invalid");
-      document
-        .querySelector(`.${parentClass.classList[0]} + p`)
-        .classList.add("message");
-      document.querySelector(`.${parentClass.classList[0]} + p`).textContent =
-        "Your password is weak";
-    } else {
-      const parentClass = passwordInp.parentElement;
-      parentClass.classList.remove("invalid");
-      document
-        .querySelector(`.${parentClass.classList[0]} + p`)
-        .classList.remove("message");
-    }
+    [...passwordInp.value].length <= 8
+      ? invalidInput("Your password is weak", passwordInp.parentElement)
+      : validInput(passwordInp.parentElement);
   }
 });
